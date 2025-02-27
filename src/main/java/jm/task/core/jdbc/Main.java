ecd4bfa0532f1eaca67c42b4246941ddeb1b6626
Alim <abbeyrus@student.21-school.ru>
2025-02-27 21:53:36 +0500

    v1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,36 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("Name1", "LastName1", (byte) 20);
+        System.out.println("User с именем — LastName1 добавлен в базу данных");
+        userDao.saveUser("Name2", "LastName2", (byte) 25);
+        System.out.println("User с именем — LastName2 добавлен в базу данных");
+        userDao.saveUser("Name3", "LastName3", (byte) 31);
+        System.out.println("User с именем — LastName3 добавлен в базу данных");
+        userDao.saveUser("Name4", "LastName4", (byte) 38);
+        System.out.println("User с именем — LastName4 добавлен в базу данных");
+
+        List<User> allUsers = userDao.getAllUsers();
+        for (User user : allUsers) {
+            System.out.println(user.toString());
+        }
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
+
+
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static String dbURL = "jdbc:mysql://localhost:3306/db_test";;
+    private static String dbUsername = "root";
+    private static String dbPassword = "";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
