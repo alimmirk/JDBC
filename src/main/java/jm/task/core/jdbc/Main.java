d452f4b1a27ec4decddc7ec58cc0df7ce188a95b
Alim <abbeyrus@student.21-school.ru>
2025-03-05 23:09:03 +0500

    update v2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,6 +3,8 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
@@ -11,25 +13,25 @@ public class Main {
 
     public static void main(String[] args) {
         Util.getConnection();
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserService userService = new UserServiceImpl();
 
-        userDao.createUsersTable();
+        userService.createUsersTable();
 
-        userDao.saveUser("Name1", "LastName1", (byte) 20);
-        System.out.println("User с именем — LastName1 добавлен в базу данных");
-        userDao.saveUser("Name2", "LastName2", (byte) 25);
-        System.out.println("User с именем — LastName2 добавлен в базу данных");
-        userDao.saveUser("Name3", "LastName3", (byte) 31);
-        System.out.println("User с именем — LastName3 добавлен в базу данных");
-        userDao.saveUser("Name4", "LastName4", (byte) 38);
-        System.out.println("User с именем — LastName4 добавлен в базу данных");
+        userService.saveUser("Name1", "LastName1", (byte) 20);
+        //System.out.println("User с именем — LastName1 добавлен в базу данных");
+        userService.saveUser("Name2", "LastName2", (byte) 25);
+        //System.out.println("User с именем — LastName2 добавлен в базу данных");
+        userService.saveUser("Name3", "LastName3", (byte) 31);
+        //System.out.println("User с именем — LastName3 добавлен в базу данных");
+        userService.saveUser("Name4", "LastName4", (byte) 38);
+        //System.out.println("User с именем — LastName4 добавлен в базу данных");
 
-        List<User> allUsers = userDao.getAllUsers();
+        List<User> allUsers = userService.getAllUsers();
         for (User user : allUsers) {
             System.out.println(user.toString());
         }
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 
 
